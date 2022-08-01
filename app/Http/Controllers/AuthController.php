cf7398dbbb7e496682d7713fb92a823c0fb2303f
evren Önen <evr.onen@gmail.com>
2022-08-01 10:30:14 +0300

    starting lizbon

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,114 @@
+<?php
+namespace app\Models;
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Http\Request;
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Models\User;
+
+
+class AuthController extends Controller
+{
+  /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+    public function register()
+    {
+        $validator= validator()->make(request()->all(),[
+            'name'      => 'required | string',
+            'email'     => 'required | email',
+            'password'  => 'required | string | min:6'
+        ]);
+
+        if($validator->fails()){
+            return response()-> json([
+                'message'   => request()-> get('name'). ' '. request()-> get('email'). ' ' . request()-> get('password')
+            ]);
+        }
+        $user = User::create([
+            'name'      => request()-> get('name'),
+            'email'     => request()-> get('email'),
+            'password'  => bcrypt( request()-> get('password'))
+        ]);
+return response()-> json([
+    'message'   => 'User Created',
+    'user'      => $user
+]);
+}
+
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+        
+        if (! $token = auth()->claims(['nam'=> 'Evren'])->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized a'], 401);
+        }
+
+        return $this->respondWithToken(['token' => $token,  'payloads' => auth()->payload()]);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 600000
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
+namespace App\Http\Controllers;
+
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'namespace' => 'App\Http\Controllers',
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+Route::group([
+    'namespace' => 'App\Http\Controllers',
+    'middleware' => 'api',
+    'prefix' => 'product'
+
+], function ($router) {
+
+    Route::post('create', 'ProductsController@create_product');
+    Route::post('edit', 'ProductsController@update_product');
+    Route::get('get/{stokkod}', 'ProductsController@get_product');
+    Route::get('/', 'ProductsController@get_products');
+    
+
 });
